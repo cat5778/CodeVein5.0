1621b48e3f5816e51c324eac34bd3fe65d7f417a
dhdld2528 <dhdld2528@DESKTOP-LVHRP7G>
2020-06-29 05:13:43 +0900

    Init

---
+++ 64FrameWork/Client/Code/Effect.cpp
@@ -0,0 +1,147 @@
+#include "stdafx.h"
+#include "Effect.h"
+
+#include "Export_Function.h"
+
+CEffect::CEffect(LPDIRECT3DDEVICE9 pGraphicDev)
+	: Engine::CGameObject(pGraphicDev)
+{
+
+}
+
+CEffect::~CEffect(void)
+{
+
+}
+
+HRESULT CEffect::Ready_GameObject(void)
+{
+	FAILED_CHECK_RETURN(Add_Component(), E_FAIL);
+
+	m_pTransformCom->Set_Pos(_float(rand() % 20), 0.f, _float(rand() % 20));
+
+	return S_OK;
+}
+
+_int CEffect::Update_GameObject(const _float& fTimeDelta)
+{
+	m_fFrameCnt += m_fFrameMax * fTimeDelta;
+
+	if (m_fFrameCnt >= m_fFrameMax)
+		m_fFrameCnt = 0.f;
+
+
+	Engine::CGameObject::Update_GameObject(fTimeDelta);	
+
+	_matrix	matWorld, matView, matBill;
+	
+	m_pTransformCom->Get_WorldMatrix(&matWorld);
+	m_pGraphicDev->GetTransform(D3DTS_VIEW, &matView);
+
+	D3DXMatrixIdentity(&matBill);
+
+	matBill._11 = matView._11;
+	matBill._13 = matView._13;
+	matBill._31 = matView._31;
+	matBill._33 = matView._33;
+
+	D3DXMatrixInverse(&matBill, NULL, &matBill);
+
+	m_pTransformCom->Set_WorldMatrix(&(matBill * matWorld));
+
+	Engine::CGameObject::Compute_ViewZ(&m_pTransformCom->m_vInfo[Engine::INFO_POS]);
+
+	m_pRendererCom->Add_RenderGroup(Engine::RENDER_ALPHA, this);
+
+	return 0;
+}
+
+void CEffect::Render_GameObject(void)
+{
+
+	LPD3DXEFFECT	pEffect = m_pShaderCom->Get_EffectHandle();
+	NULL_CHECK(pEffect);
+	pEffect->AddRef();
+
+	_uint	iPassMax = 0;
+
+	SetUp_ConstantTable(pEffect);
+
+	pEffect->Begin(&iPassMax, 0);
+
+	pEffect->BeginPass(0);
+
+	m_pBufferCom->Render_Buffer();
+
+	pEffect->EndPass();
+
+	pEffect->End();
+
+	Safe_Release(pEffect);
+}
+
+HRESULT CEffect::Add_Component(void)
+{
+	Engine::CComponent*		pComponent = nullptr;
+
+	pComponent = m_pBufferCom = dynamic_cast<Engine::CRcTex*>(Engine::Clone(RESOURCE_STATIC, L"Buffer_RcTex"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_pComponentMap[Engine::ID_STATIC].emplace(L"Com_Buffer", pComponent);
+
+	pComponent = m_pTransformCom = Engine::CTransform::Create();
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_pComponentMap[Engine::ID_DYNAMIC].emplace(L"Com_Transform", pComponent);
+
+	pComponent = m_pTextureCom = dynamic_cast<Engine::CTexture*>(Engine::Clone(RESOURCE_STAGE, L"Texture_Effect"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_pComponentMap[Engine::ID_STATIC].emplace(L"Com_Texture", pComponent);
+
+	pComponent = m_pRendererCom = Engine::Get_Renderer();
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	pComponent->AddRef();
+	m_pComponentMap[Engine::ID_STATIC].emplace(L"Com_Renderer", pComponent);
+	
+	pComponent = m_pShaderCom = dynamic_cast<Engine::CShader*>(Engine::Clone_Prototype(L"Shader_Effect"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_pComponentMap[Engine::ID_STATIC].emplace(L"Com_Shader", pComponent);
+	
+	return S_OK;
+}
+
+
+
+HRESULT CEffect::SetUp_ConstantTable(LPD3DXEFFECT& pEffect)
+{
+	_matrix			matWorld, matView, matProj;
+
+	m_pTransformCom->Get_WorldMatrix(&matWorld);
+	m_pGraphicDev->GetTransform(D3DTS_VIEW, &matView);
+	m_pGraphicDev->GetTransform(D3DTS_PROJECTION, &matProj);
+
+	pEffect->SetMatrix("g_matWorld", &matWorld);
+	pEffect->SetMatrix("g_matView", &matView);
+	pEffect->SetMatrix("g_matProj", &matProj);
+
+	m_pTextureCom->Set_Texture(pEffect, "g_BaseTexture", _uint(m_fFrameCnt));
+	Engine::SetUp_OnShader(pEffect, L"Target_Depth", "g_DepthTexture");
+
+	return S_OK;
+}
+
+CEffect* CEffect::Create(LPDIRECT3DDEVICE9 pGraphicDev)
+{
+	CEffect*	pInstance = new CEffect(pGraphicDev);
+
+	if (FAILED(pInstance->Ready_GameObject()))
+		Engine::Safe_Release(pInstance);
+
+	return pInstance;
+}
+
+void CEffect::Free(void)
+{
+
+
+	Engine::CGameObject::Free();
+}
+
